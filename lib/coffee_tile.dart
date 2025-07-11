4d4235a903c33ae479ae97ff6d4a1bd95902d107
mayurranshinge08 <mayurranshinge08@gmail.com>
2025-07-11 16:21:40 +0530

    push request

--- lib/coffee_tile.dart
+++ lib/coffee_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class CoffeeTile extends StatelessWidget {
   const CoffeeTile({
     required this.coffeeImagePath,
@@ -7,41 +8,49 @@ class CoffeeTile extends StatelessWidget {
     required this.coffeeSub,
   });
 
-final String coffeeImagePath;
+  final String coffeeImagePath;
   final String coffeeName;
   final String coffeePrice;
   final String coffeeSub;
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.only(left: 25.0, bottom: 25),
       child: Container(
         padding: EdgeInsets.all(12),
         width: 200,
         decoration: BoxDecoration(
-          color: Colors.black54 ,
-          borderRadius: BorderRadius.circular(12 ),
+          color: Colors.black54,
+          borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(coffeeImagePath),),
-           Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 12.0),
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text(coffeeName,style: TextStyle(fontSize: 20),),
-                 Text(coffeeSub,style: TextStyle(color: Colors.grey.shade700),),
-
-               ],
-             ),
-           ),
-
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(coffeeImagePath),
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0,),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    coffeeName,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    coffeeSub,
+                    style: TextStyle(color: Colors.grey.shade700),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+              ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -50,9 +59,10 @@ final String coffeeImagePath;
                     padding: EdgeInsets.all(4),
                     decoration: BoxDecoration(
                       color: Colors.orange,
-                      borderRadius: BorderRadius.circular(6)
+                      borderRadius: BorderRadius.circular(6),
                     ),
-                      child: Icon(Icons.add),),
+                    child: Icon(Icons.add),
+                  ),
                 ],
               ),
             )
